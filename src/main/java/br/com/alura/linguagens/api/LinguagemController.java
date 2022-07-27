a22ef68cc7f182833876acbccf89a89cbf695102
alemarcos5 <alemarcos5@gmail.com>
2022-07-26 22:49:15 -0300

    adicionando endpoint para metodo delete

--- src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -3,7 +3,9 @@ package br.com.alura.linguagens.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +32,12 @@ public class LinguagemController {
         System.out.println(linguagem.getTitle());
         return repositorio.save(linguagem);
     }
+
+    @DeleteMapping(value = "/linguagens/{id}")
+    public void deletarLinguagem(@PathVariable String id){
+        System.out.println(id);
+        repositorio.deleteById(id);
+    }
+
 
 }
